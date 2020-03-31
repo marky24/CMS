4581bb65a5cfc4973d698c7025218642cfc23bcf
marky24 <marky24@mail.ru>
2020-03-31 03:18:27 +0300

    Create index.php

---
+++ 4dz/index.php
@@ -0,0 +1,72 @@
+<head>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+<?php
+get_header();
+?>
+<div class="container mx-auto" align="center">
+	<i class="fa fa-ship"></i>
+	<strong> logo</strong>
+</div>
+<div class = "mx-auto">
+	<h1 align="center">
+		<strong>
+			Join the world’s largest startup community
+		</strong> 
+	</h1>
+</div>
+<div class="container">
+	<h2 align="center">Experience the startup ecosystem — invest in startups, research the fastest-growing companies, and find a job you love.</h2>
+</div>
+<div class="container mt-5">
+   <div class="row"> 
+      <div class="col">
+      	<h3>
+      		<strong>
+      			AngelList Talent	
+      		</strong>
+      		<i class="fa fa-search" aria-hidden="true"></i>
+      	</h3>
+		<h4>
+			Apply privately to 130,000+ tech & startup jobs with one application. See salary and equity upfront.
+		</h4>
+		<button type="button" class="btn btn-primary">Find startup jobs	</button>
+      </div> 
+    
+      <div class="col">
+      	<h3>
+      		<strong>
+      			AngelList Venture
+      		</strong>
+      		<i class="fa fa-bar-chart" aria-hidden="true"></i>
+      	</h3>
+      	<h4>
+      		Over $1 billion deployed alongside leading venture fund managers with 36% of all top-tier U.S. VC deals funded on AngelList.
+      	</h4>
+      	<button type="button" class="btn btn-primary">Apply to be investor</button>
+      </div> 
+ 
+      <div class="col">
+      	<h3>
+      		<strong>
+      			AngelList Venture
+      		</strong>
+      		<i class="fa fa-rocket" aria-hidden="true"></i>
+      	</h3>
+      	<h4>
+      		The next big thing in tech launches every day. A friendly community of millions of makers & early adopters awaits.
+      	</h4>
+      	<button type="button" class="btn btn-primary">Explore products</button>
+      </div> 
+   </div> 
+</div>
+</div>
+<?php
+//get_sidebar();
+//get_footer();
+?>
