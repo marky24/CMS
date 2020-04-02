0d39640b4457a568a021896cc51007e7cae3fea4
marky24 <marky24@mail.ru>
2020-04-02 22:48:40 +0300

    Update index.php

--- 4dz/index.php
+++ 4dz/index.php
@@ -1,72 +1,166 @@
 <head>
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-<?php
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <link href="wp-content/themes/sova_base_theme/css/bootstrap.css" rel="stylesheet" />
+    <link href="wp-content/themes/sova_base_theme/css/custom.css" rel="stylesheet" />
+    <?php
 get_header();
 ?>
+</head>
 <div class="container mx-auto" align="center">
-	<i class="fa fa-ship"></i>
-	<strong> logo</strong>
+    <i class="fa fa-ship"></i>
+    <strong> logo</strong>
 </div>
-<div class = "mx-auto">
-	<h1 align="center">
-		<strong>
-			Join the world’s largest startup community
-		</strong> 
-	</h1>
+<div class="mx-auto">
+    <h1 align="center">
+        <strong>
+            Join the world’s largest startup community
+        </strong>
+    </h1>
 </div>
 <div class="container">
-	<h2 align="center">Experience the startup ecosystem — invest in startups, research the fastest-growing companies, and find a job you love.</h2>
+    <h2 align="center">Experience the startup ecosystem — invest in startups, research the fastest-growing companies, and find a job you love.</h2>
 </div>
-<div class="container mt-5">
-   <div class="row"> 
-      <div class="col">
-      	<h3>
-      		<strong>
-      			AngelList Talent	
-      		</strong>
-      		<i class="fa fa-search" aria-hidden="true"></i>
-      	</h3>
-		<h4>
-			Apply privately to 130,000+ tech & startup jobs with one application. See salary and equity upfront.
-		</h4>
-		<button type="button" class="btn btn-primary">Find startup jobs	</button>
-      </div> 
-    
-      <div class="col">
-      	<h3>
-      		<strong>
-      			AngelList Venture
-      		</strong>
-      		<i class="fa fa-bar-chart" aria-hidden="true"></i>
-      	</h3>
-      	<h4>
-      		Over $1 billion deployed alongside leading venture fund managers with 36% of all top-tier U.S. VC deals funded on AngelList.
-      	</h4>
-      	<button type="button" class="btn btn-primary">Apply to be investor</button>
-      </div> 
- 
-      <div class="col">
-      	<h3>
-      		<strong>
-      			AngelList Venture
-      		</strong>
-      		<i class="fa fa-rocket" aria-hidden="true"></i>
-      	</h3>
-      	<h4>
-      		The next big thing in tech launches every day. A friendly community of millions of makers & early adopters awaits.
-      	</h4>
-      	<button type="button" class="btn btn-primary">Explore products</button>
-      </div> 
-   </div> 
+<div class="container mt-5 " >
+    <div class="row">
+        <div class="col">
+            <h3>
+                <strong>
+                    AngelList Talent
+                </strong>
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </h3>
+            <h4>
+                Apply privately to 130,000+ tech & startup jobs with one application. See salary and equity upfront.
+            </h4>
+        </div>
+        <div class="col">
+            <h3>
+                <strong>
+                    AngelList Venture
+                </strong>
+                <i class="fa fa-bar-chart" aria-hidden="true"></i>
+            </h3>
+            <h4>
+                Over $1 billion deployed alongside leading venture fund managers with 36% of all top-tier U.S. VC deals funded on AngelList.
+            </h4>
+        </div>
+        <div class="col">
+            <h3>
+                <strong>
+                    AngelList Venture
+                </strong>
+                <i class="fa fa-rocket" aria-hidden="true"></i>
+            </h3>
+            <h4>
+                The next big thing in tech launches every day. A friendly community of millions of makers & early adopters awaits.
+            </h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <button type="button" class="btn btn-primary">Find startup jobs </button>
+        </div>
+        <div class="col">
+            <button type="button" class="btn btn-primary">Apply to be investor</button>
+        </div>
+        <div class="col">
+            <button type="button" class="btn btn-primary">Explore products</button>
+        </div>
+    </div>
 </div>
+<div class="container mt-6">
+    <div class="row">
+        <div class="col">
+            <h3 class=" container mt-5">
+                <strong>
+                    AngelList Talent
+                </strong>
+            </h3>
+            <h1 class="container mt-3">
+                <strong>
+                    Grow your career
+                </strong>
+            </h1>
+            <h2 class="container mt-3">
+                Twitch, Stripe, and over 100,000 startups of all sizes have used AngelList to hire people like you. Already know where you want to be? You can apply directly—and privately—to early stage startups and industry leaders alike.
+            </h2>
+            <div class="container row mt-5">
+                <div class="col">
+                    <h5>
+                        <strong>
+                            Talk directly to founders
+                        </strong>
+                    </h5>
+                </div>
+                <div class="col">
+                    <h5>
+                        <strong>
+                            See salary & equity upfront
+                        </strong>
+                    </h5>
+                </div>
+            </div>
+            <div class="container row mt-1">
+                <div class="col">
+                    Speak directly to founders and hiring managers. No third party recruiters allowed.
+                </div>
+                <div class="col">
+                    View salary and stock option details before you apply to a job.
+                </div>
+            </div>
+            <div class="container row mt-3">
+                <div class="col">
+                    <button type="button" class="btn btn-primary">Find startup jobs</button>
+                    <button type="button" class="btn btn-light">Post a job</button>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <img src="http://localhost/sova30.04/wp-content/themes/sova_base_theme/roma.jpg" alt="Roma" class="img-fluid">
+        </div>
+    </div>
+</div>
+<div class="container mt-6" align="center">
+    <h3>
+        Explore over 2 million tech and startup job-opportunities
+    </h3>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col border border-dark mr-3 rounded">
+
+            <h4>
+                <strong>
+                    Software Engineer at Twitch
+                </strong>
+            </h4>
+            <p>$130k – $210k • 0.0% – 0.001%</p>
+        </div>
+        <div class="col border border-dark mr-3 ml-3 rounded">
+            <h4>
+                <strong>Software Engineer at Uber</strong>
+            </h4>
+            <p>$100k – $170k</p>
+        </div>
+        <div class="col border border-dark ml-3 rounded">
+            <h4> <strong>Product Manager at Roblox </strong></h4>
+            <p>$115k – $165k%</p>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col border border-dark mr-3 rounded">
+            <h4> <strong>Senior Product Designer at Instacart</strong></h4>
+            <p>$110k – $140k • 0.0% – 0.001%</p>
+        </div>
+        <div class="col border border-dark ml-3 rounded">
+            <h4> <strong>Data Science Manager at Discord </strong></h4>
+            <p>$180k – $220k • 0.005% – 0.05%</p>
+        </div>
+    </div>
 </div>
 <?php
-//get_sidebar();
-//get_footer();
+get_footer();
 ?>
