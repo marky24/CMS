3b94b351016567697027342e494f19070cd42afa
marky24 <marky24@mail.ru>
2020-04-09 09:54:17 +0300

    Update index.php

--- 2d_dz/index.php
+++ 2d_dz/index.php
@@ -1,232 +1,72 @@
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    <link href="assets/bootstrap.css" rel="stylesheet" />
-    <link href="assets/custom.css" rel="stylesheet" />
-    <style>
-        #red {
-            background-color: #FF0000;
-            border: none;
-        }
-
-        #purple {
-            background-color: #9900FF;
-            border: none;
-        }
-
-        #blue {
-            background-color: #0000FF;
-            border: none;
-        }
-
-        .big_header {
-            font-size: 80px;
-        }
-    </style>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <!-- <a class="navbar-brand" href="#"><i class="fa fa-signal"></i></a> -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse container" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a href="#">
-                        <i class="fa fa-signal fa-2x"></i>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Startup jobs</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Recruit</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Invest</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Blog</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        More
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Venture funds</a>
-                        <a class="dropdown-item" href="#">Companies</a>
-                        <a class="dropdown-item" href="#">Incubators</a>
-                        <a class="dropdown-item" href="#">Track</a>
-                        <a class="dropdown-item" href="#">Salaries</a>
-                        <a class="dropdown-item" href="#">Valuations</a>
-                        <a class="dropdown-item" href="#">Help</a>
-                    </div>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-            <a class="nav-link" href="#">Join</a>
-            <a class="nav-link" href="#">Login</a>
-        </div>
-    </nav>
-</head>
-<div class="container mt-5" align="center">
-    <i class="fa fa-ship"></i>
-    <strong> logo</strong>
-</div>
-<div class="container pl-3 pr-3">
-    <div class="container">
-        <h1 class= "big_header" align="center">
-            <strong>
-                Join the worlds largest </br> startup community
-            </strong>
-        </h1>
-    </div>
-    <div class="container">
-        <h5 align="center">Experience the startup ecosystem - invest in startups, research the </br> fastest-growing companies, and find a job you love.</h5>
-    </div>
-</div>
-<div class="container mt-5 ">
-    <div class="row">
-        <div class="col mr-3">
-            <h3>
-                <strong>
-                    AngelList Talent
-                </strong>
-                <i class="fa fa-search float-right" aria-hidden="true"></i>
-            </h3>
-            <h4>
-                Apply privately to 130,000+ tech & startup jobs with one application. See salary and equity upfront.
-            </h4>
-        </div>
-        <div class="col mr-3">
-            <h3>
-                <strong>
-                    AngelList Venture
-                </strong>
-                <i align="right" class="fa fa-bar-chart float-right" aria-hidden="true"></i>
-            </h3>
-            <h4>
-                Over $1 billion deployed alongside leading venture fund managers with 36% of all top-tier U.S. VC deals funded on AngelList.
-            </h4>
-        </div>
-        <div class="col">
-            <h3>
-                <strong>
-                    Product Hunt
-                </strong>
-                <i align="right" class="fa fa-rocket float-right" aria-hidden="true"></i>
-            </h3>
-            <h4>
-                The next big thing in tech launches every day. A friendly community of millions of makers & early adopters awaits.
-            </h4>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <button type="button" class="btn btn-primary" id="blue">Find startup jobs </button>
-        </div>
-        <div class="col">
-            <button type="button" class="btn btn-primary" id="purple">Apply to be investor</button>
-        </div>
-        <div class="col">
-            <button type="button" class="btn btn-primary" id="red">Explore products</button>
-        </div>
-    </div>
-</div>
-<div class="container mt-6">
-    <div class="row">
-        <div class="col">
-            <h3 class=" container mt-5">
-                <strong>
-                    AngelList Talent
-                </strong>
-            </h3>
-            <h1 class="container mt-3">
-                <strong>
-                    Grow your career
-                </strong>
-            </h1>
-            <h2 class="container mt-3">
-                Twitch, Stripe, and over 100,000 startups of all sizes have used AngelList to hire people like you. Already know where you want to be? You can apply directly—and privately—to early stage startups and industry leaders alike.
-            </h2>
-            <div class="container row mt-5">
-                <div class="col">
-                    <h5>
-                        <strong>
-                            Talk directly to founders
-                        </strong>
-                    </h5>
-                </div>
-                <div class="col">
-                    <h5>
-                        <strong>
-                            See salary & equity upfront
-                        </strong>
-                    </h5>
-                </div>
-            </div>
-            <div class="container row mt-1">
-                <div class="col">
-                    Speak directly to founders and hiring managers. No third party recruiters allowed.
-                </div>
-                <div class="col">
-                    View salary and stock option details before you apply to a job.
-                </div>
-            </div>
-            <div class="container row mt-3">
-                <div class="col">
-                    <button type="button" class="btn btn-primary">Find startup jobs</button>
-                    <button type="button" class="btn btn-light">Post a job</button>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <img src="http://localhost/sova30.04/wp-content/themes/sova_base_theme/roma.jpg" alt="Roma" class="img-fluid">
-        </div>
-    </div>
-</div>
-<div class="container mt-6" align="center">
-    <h3>
-        Explore over 2 million tech and startup job-opportunities
-    </h3>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col border border-dark mr-3 rounded">
-            <h4>
-                <strong>
-                    Software Engineer at Twitch
-                </strong>
-            </h4>
-            <p>$130k – $210k • 0.0% – 0.001%</p>
-        </div>
-        <div class="col border border-dark mr-3 ml-3 rounded">
-            <h4>
-                <strong>Software Engineer at Uber</strong>
-            </h4>
-            <p>$100k – $170k</p>
-        </div>
-        <div class="col border border-dark ml-3 rounded">
-            <h4> <strong>Product Manager at Roblox </strong></h4>
-            <p>$115k – $165k%</p>
-        </div>
-    </div>
-    <div class="row mt-3">
-        <div class="col border border-dark mr-3 rounded">
-            <h4> <strong>Senior Product Designer at Instacart</strong></h4>
-            <p>$110k – $140k • 0.0% – 0.001%</p>
-        </div>
-        <div class="col border border-dark ml-3 rounded">
-            <h4> <strong>Data Science Manager at Discord </strong></h4>
-            <p>$180k – $220k • 0.005% – 0.05%</p>
-        </div>
-    </div>
-</div>
+    <form action="create.php" method="post">
+        <input type="submit" value="Создать таблицу">
+    </form>
 <?php
-get_footer();
-?>
+            //echo "im in";
+            $servername = "localhost";
+            $username = "test";
+            $password = "qwerty123456";
+            $db_name = "2dz";
+
+            $conn = new mysqli($servername, $username, $password, $db_name);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } 
+
+            $sql = "SELECT name, surname, phone FROM Persons";
+            $result = $conn->query($sql);
+    ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+    <table id="example" class="display" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>Phone</th>
+                <th>Delete button</th>
+                <!--<th>time_post</th>-->
+            </tr>
+        </thead>
+        <tbody>
+            <?php while($row = $result->fetch_assoc()) {?>
+            <tr>
+                <? echo "keker"; ?>
+                <td align="center">
+                    <?php echo $row["name"];?>
+                </td>
+                <td align="center">
+                    <?php echo $row["surname"];?>
+                </td>
+                <td align="center">
+                    <?php echo $row["phone"];?>
+                </td>
+                <td align="center">
+                    <form action="delete_post.php" method="post">
+                        <input type='hidden' name='phone' value='<?php echo $row["phone"];?>'>
+                        <input type="submit" value="Удалить">
+                    </form>
+                </td>
+            </tr>
+            <?php };?>
+        </tbody>
+    </table>
+    <br />
+    <form action="send_post.php" method="post">
+        Name: <input type="text" name="name">
+        Surname: <input type="text" name="surname">
+        Phone: <input type="text" name="phone">
+        <input type="submit" value="Добавить">
+    </form>
+    <form action="delete.php" method="post">
+        <input type="submit" value="Удалить таблицу">
+    </form>
+</body>
+
+</html>
